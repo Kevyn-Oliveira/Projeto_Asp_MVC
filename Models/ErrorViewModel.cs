ceeafafe9e6630017df263319770217cedac2d95
Kevyn de Oliveira <kdokevyn@gmail.com>
2024-01-23 18:43:52 -0300

    Criação do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoAspMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
