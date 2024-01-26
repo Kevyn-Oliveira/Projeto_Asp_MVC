866ebf5a17c0064c45cb8aa5c38b5b9ab01cef7f
Kevyn de Oliveira <kdokevyn@gmail.com>
2024-01-25 21:29:17 -0300

    Criação da pagina de videos e de analises

--- Models/Noticia.cs
+++ Models/Noticia.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata;
 using System.Runtime.Serialization;
 
 namespace ProjetoAspMvc.Models
@@ -14,5 +15,9 @@ namespace ProjetoAspMvc.Models
         [Column("Titulo")]
         [Display(Name = "Titulo")]
         public string TituloNoticia { get; set; }
+
+        [Column("Imagem")]
+        [Display(Name = "Imagem")]
+        public Blob ImagemNoticia { get; set; }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function Destacar(objeto) {
+    objeto.style = "border: 2px solid red;";
+}
+
+function Desdestacar(objeto) {
+    objeto.style = "border: none;"
+}
